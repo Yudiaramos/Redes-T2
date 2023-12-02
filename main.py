cca789da7f1d0d7539c66537bbe6dba4b5164745
Yudi Asano Ramos <71808184+Yudiaramos@users.noreply.github.com>
2023-12-01 23:19:23 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ def config():
     global porcentagemDeErros, enquadramento, tipoDeControleDeErro
     
     # Configurações iniciais do sistema
-    porcentagemDeErros = int(input("Qual a porcentagem de erros do meio de comunicação? (Número inteiro entre 0 e 100): "))
-    enquadramento = int(input("Enquadramento? (0 para contagem de caracteres, 1 para inserção de bytes): "))
-    tipoDeControleDeErro = int(input("Detecção de erros? (0 para bit de paridade par, 1 para bit de paridade ímpar, 2 para CRC): "))
+    porcentagemDeErros = int(input(" inteiro entre 0 e 100: "))
+
+    enquadramento = int(input("0 para contagem de caracteres, 1 para insercao: "))
+
+    tipoDeControleDeErro = int(input("0 para par, 1 para ímpar, 2 para CRC: "))
+
 
 def meio_de_comunicacao(fluxo_bruto_de_bits):
-    print("\nMeio de Comunicação:\n")
-    print(f"Quadro original: {fluxo_bruto_de_bits}")
+    print(f"mensagem original: {fluxo_bruto_de_bits}")
     
     fluxo_bruto_ponto_a = list(fluxo_bruto_de_bits)
     fluxo_bruto_ponto_b = []
@@ -28,22 +30,16 @@ def meio_de_comunicacao(fluxo_bruto_de_bits):
         else:
             fluxo_bruto_ponto_b.append(1 - bit)
 
-    print(f"Quadro após simulação de meio de comunicação: {fluxo_bruto_ponto_b}\n")
+    print(f"Quadro após: {fluxo_bruto_ponto_b}\n")
 
     # Próxima camada: Recebimento na Camada Física
     camada_fisica_receptora(fluxo_bruto_ponto_b)
 
 def camada_fisica_receptora(quadro):
-    print("\nCamada Física Receptora:\n")
-    print("Recebendo...\n")
-
     # Próxima camada: Camada de Enlace de Dados
     camada_enlace_dados_receptora(quadro)
 
 def camada_enlace_dados_receptora(quadro):
-    print("\nCamada de Enlace Receptora:\n")
-    print("Desenquadra e corrige erros...\n")
-
     # Controle de erro e desenquadramento
     quadro_corrigido = camada_enlace_dados_receptora_controle_de_erro(quadro)
     quadro_desenquadrado = camada_enlace_dados_receptora_enquadramento(quadro_corrigido)
@@ -52,7 +48,6 @@ def camada_enlace_dados_receptora(quadro):
     camada_de_aplicacao_receptora(quadro_corrigido)
 
 def camada_enlace_dados_receptora_enquadramento(quadro):
-    print("\nDesenquadramento:\n")
 
     if enquadramento == 0:
         return camada_enlace_dados_receptora_enquadramento_contagem_de_caracteres(quadro)
@@ -60,13 +55,10 @@ def camada_enlace_dados_receptora_enquadramento(quadro):
         return camada_enlace_dados_receptora_enquadramento_insercao_de_bytes(quadro)
 
 def camada_enlace_dados_receptora_enquadramento_contagem_de_caracteres(quadro):
-    print("Por Contagem de Caracteres:\n")
-    print("Enquadrando...\n")
+
     return quadro[8:]
 
 def camada_enlace_dados_receptora_enquadramento_insercao_de_bytes(quadro):
-    print("Por Inserção de Bytes:\n")
-    print("Desenquadrando...\n")
     
     byte_str = ""
     quadro_str = ""
@@ -95,7 +87,7 @@ def camada_enlace_dados_receptora_enquadramento_insercao_de_bytes(quadro):
     return novo_quadro
 
 def camada_enlace_dados_receptora_controle_de_erro(quadro):
-    print("\nControle de Erro:\n")
+
 
     if tipoDeControleDeErro == 0:
         return camada_enlace_dados_receptora_controle_de_erro_bit_paridade_par(quadro)
@@ -105,36 +97,20 @@ def camada_enlace_dados_receptora_controle_de_erro(quadro):
         return camada_enlace_dados_receptora_controle_de_erro_crc(quadro)
 
 def camada_enlace_dados_receptora_controle_de_erro_bit_paridade_par(quadro):
-    print("Por Bit de Paridade Par:\n")
-    print("Corrigindo...\n")
-
     recebimento_paridade_par = quadro[:-1]
     paridade = sum(recebimento_paridade_par) % 2 == 0
-
-    if quadro[-1] == paridade:
-        print("\nRecebeu com sucesso!\n")
-    else:
-        print("\nProblema na comunicação detectado!\n")
 
     return recebimento_paridade_par
 
 def camada_enlace_dados_receptora_controle_de_erro_bit_paridade_impar(quadro):
-    print("Por Bit de Paridade Ímpar:\n")
-    print("Corrigindo...\n")
+
 
     recebimento_paridade_impar = quadro[:-1]
     paridade = sum(recebimento_paridade_impar) % 2 == 1
 
-    if quadro[-1] == paridade:
-        print("\nRecebeu com sucesso!\n")
-    else:
-        print("\nProblema na comunicação detectado!\n")
-
     return recebimento_paridade_impar
 
 def camada_enlace_dados_receptora_controle_de_erro_crc(quadro):
-    print("Por CRC:\n")
-    print("Inserindo correção...\n")
 
     polinomio_crc_32 = [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1]
     mensagem = quadro[:-31]
@@ -150,16 +126,10 @@ def camada_enlace_dados_receptora_controle_de_erro_crc(quadro):
         if novo_quadro[i] != 0:
             valido = False
 
-    if not valido:
-        print("\nErro!\n")
-    else:
-        print("\nRecebeu com sucesso!\n")
-
     return mensagem
 
 def camada_de_aplicacao_receptora(quadro):
-    print("\nCamada de Aplicação Receptora:\n")
-    print("Transferindo fluxo de bits para mensagem legível...\n")
+
 
     mensagem = decode_to_string(quadro)
     aplicacao_receptora(mensagem)
@@ -181,11 +151,10 @@ def decode_to_string(quadro):
     return mensagem
 
 def aplicacao_receptora(mensagem):
-    print("\nAplicação Receptora:\n")
     print(f"A mensagem recebida foi: {mensagem}\n")
 
 def aplicacao_transmissora():
-    print("\nInício:\n\n")
+
     config()
     mensagem = input("Digite uma mensagem: ")
     camada_de_aplicacao_transmissora(mensagem)
@@ -196,7 +165,7 @@ def camada_de_aplicacao_transmissora(mensagem):
     
     # Próxima camada: Codificação para Bits
     quadro = encode_to_bits(mensagem)
-    print(f"O quadro gerado é: {quadro}\n")
+    print(f"quadro: {quadro}\n")
     
     # Próxima camada: Meio de Comunicação
     meio_de_comunicacao(quadro)
